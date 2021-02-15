e471caa2e6d7f28526a27960e5427c3c3fdf5ca9
jgarmos <javiergarciaramos@gmail.com>
2021-02-15 22:39:25 +0000

    added delete a note

--- src/containers/Notes.js
+++ src/containers/Notes.js
@@ -87,7 +87,12 @@ export default function Notes() {
         }
     }
 
+    function deleteNote() {
+        return API.del("notes", `/notes/${id}`);
+    }
+
     async function handleDelete(event) {
+        debugger;
         event.preventDefault();
         const confirmed = window.confirm(
             "Are you sure you want to delete this note?"
@@ -96,6 +101,14 @@ export default function Notes() {
             return;
         }
         setIsDeleting(true);
+
+        try {
+            await deleteNote();
+            history.push("/");
+        } catch (e) {
+            onError(e);
+            setIsDeleting(false);
+        }
     }
 
     return (
